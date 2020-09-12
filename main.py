b71b9fc3a9bdfaecd4bc4ba9990d5459eda2b38d
Alve Svarén <alve@hotmail.se>
2020-09-12 17:31:01 +0200

    Fixed spaces being replaces with "+" in expressions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import aiohttp
 from discord.ext import commands
+import multidict
+import aiohttp.payload as payload
+from urllib.parse import urlencode, quote
 
 bot = commands.Bot(command_prefix='$')
 
@@ -9,19 +12,47 @@ with open("token.txt") as file:
 
 default_data = {
     "fsize": "25px",
-    "fcolor": "cccccc", 
-    "mode": 0, 
+    "fcolor": "cccccc",
+    "mode": 0,
     "out": 1,
     "errors": 1,
     "preamble": r"\usepackage{amsmath}\usepackage{amsfonts}\usepackage{amssymb}"
 }
+
+
+class CustomFormData(aiohttp.FormData):
+    def _gen_form_urlencoded(self) -> payload.BytesPayload:
+        # form data (x-www-form-urlencoded)
+        data = []
+        for type_options, _, value in self._fields:
+            data.append((type_options['name'], value))
+
+        charset = self._charset if self._charset is not None else 'utf-8'
+
+        if charset == 'utf-8':
+            content_type = 'application/x-www-form-urlencoded'
+        else:
+            content_type = ('application/x-www-form-urlencoded; '
+                            'charset=%s' % charset)
+
+        return payload.BytesPayload(
+            urlencode(data, doseq=False, encoding=charset,
+                      quote_via=quote).encode(),
+            content_type=content_type)
+
+
 @bot.command(aliases=["l", "L"])
-async def latex(ctx: commands.Context, *, formula):
+async def latex(ctx: commands.Context, *, formula: str):
     async with aiohttp.ClientSession() as session:
+
         async with ctx.typing():
             embed = discord.Embed()
-            embed.set_author(name=ctx.message.author.display_name, icon_url=str(ctx.message.author.avatar_url))
-            async with session.post("https://www.quicklatex.com/latex3.f", data={"formula": formula.replace(" ", ""), **default_data}) as response:
+            embed.set_author(name=ctx.message.author.display_name,
+                             icon_url=str(ctx.message.author.avatar_url))
+            form_data = CustomFormData()
+            form_data.add_field("formula", formula)
+            form_data.add_fields(multidict.MultiDict(default_data))
+            async with session.post("https://www.quicklatex.com/latex3.f", data=form_data) as response:
                 formula_data = (await response.text()).splitlines()
                 if int(formula_data[0]) != 0:
                     await ctx.send("That isn't a valid latex expression")
